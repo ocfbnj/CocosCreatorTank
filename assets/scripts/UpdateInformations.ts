eb3db15aafab66caf43c97edfde041c961552913
ocfbnj <ocfbnj@qq.com>
2020-06-23 15:48:12 +0800

    添加信息区域

---
+++ assets/scripts/UpdateInformations.ts
@@ -0,0 +1,39 @@
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+    @property(cc.Prefab)
+    enemyIcon: cc.Prefab = null;
+
+    enemyIcons: cc.Node[] = [];
+
+    onLoad() {
+        let x = -4;
+        let y = 80;
+
+        for (let i = 0; i != 10; i++) {
+            for (let j = 0; j != 2; j++) {
+                let node = cc.instantiate(this.enemyIcon);
+                node.parent = this.node;
+
+                node.x = x + j * 8;
+                node.y = y - i * 8;
+
+                this.enemyIcons.push(node);
+            }
+        }
+    }
+
+    deleteOneIcon() {
+        this.enemyIcons.pop().destroy();
+    }
+
+    updatePlayerBlood(blood: number) {
+        this.node.getChildByName("player_blood").getComponent(cc.Label).string = blood.toString();
+    }
+
+    updateCurrentLevel(level: number) {
+        this.node.getChildByName("cur_level").getComponent(cc.Label).string = level.toString();
+    }
+}
